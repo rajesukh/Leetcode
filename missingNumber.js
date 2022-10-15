d85f3c868e7dcaf502373765102e41a8e0a18145
rajesukh <cparajesh2668@gmail.com>
2022-10-16 03:37:33 +0530

    MissingNumber

---
+++ missingNumber.js
@@ -0,0 +1,15 @@
+let arr =[1,2,3,4,5,6,8,9]
+
+function missingNumber (){
+
+    let numb = Math.max(...arr)
+    let totalSum = 0;
+    for(i=0;i<arr.length;i++){
+        totalSum+= arr[i]
+    
+    }
+
+return (numb*(numb+1)/2) -totalSum 
+}
+
+console.log(missingNumber(arr))
